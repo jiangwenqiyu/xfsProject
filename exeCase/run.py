aff04510a98df8640a9580abe2c2c2c8e22ef5e3
jyc <1006573469@qq.com>
2022-04-25 12:29:52 +0800

    修正测试报告目录

--- exeCase/run.py
+++ exeCase/run.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import shutil
 
 class PyConfig:
     caseInfos = None
@@ -20,20 +21,25 @@ class RunPyTest:
         else:
             os.mkdir('./static/reports')
 
+
+
         # 根据用户，创建用户自己的报告目录
         if os.path.exists('./static/reports/{}'.format(self.userid)):
-            pass
+            shutil.rmtree('./static/reports/{}'.format(self.userid))
+            os.mkdir('./static/reports/{}'.format(self.userid))
         else:
             os.mkdir('./static/reports/{}'.format(self.userid))
 
         # 判断用户下是否有临时allure目录
         if os.path.exists('./static/reports/{}/report_temp'.format(self.userid)):
-            pass
+            os.rmdir('./static/reports/{}/report_temp'.format(self.userid))
+            os.mkdir('./static/reports/{}/report_temp'.format(self.userid))
         else:
             os.mkdir('./static/reports/{}/report_temp'.format(self.userid))
 
         if os.path.exists('./static/reports/{}/report'.format(self.userid)):
-            pass
+            os.rmdir('./static/reports/{}/report'.format(self.userid))
+            os.mkdir('./static/reports/{}/report'.format(self.userid))
         else:
             os.mkdir('./static/reports/{}/report'.format(self.userid))
 
