60217c8a895d66321b8957187f4ac9211d3b37ee
jyc <1006573469@qq.com>
2022-05-31 16:38:16 +0800

    添加测试用例组功能

---
+++ common/models/CaseGroup.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:xfs123456@192.168.0.129:3306/xfstestpj"
+db = SQLAlchemy(app)
+
+
+class CaseGroup(db.Model):
+    __tablename__ = 'case_group'
+
+    id = db.Column(db.Integer, primary_key=True)
+    caseids = db.Column(db.String(255))
+    desc = db.Column(db.String(100))
+    user_id = db.Column(db.String(30))
